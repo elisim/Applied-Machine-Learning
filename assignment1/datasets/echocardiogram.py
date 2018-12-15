10920c32d98e01def136f383d2574ac7bfa9e1b5
tyitzhak <tal_yitzhak@intuit.com>
2018-12-15 21:04:00 +0200

    added echocardiogram data set

---
+++ assignment1/datasets/echocardiogram.py
@@ -0,0 +1,29 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from datasets.dataset import Dataset
+
+TRAIN_PATH = 'data/echocardiogram/echocardiogram.data'
+
+class EchocardiogramDataset(Dataset):
+    
+    def __init__(self):
+        self._raw_train_data = pd.read_csv(TRAIN_PATH, header=None)
+        
+    def get_classes(self):
+        return ['0','1']
+    
+    def get_train_and_test_data(self):
+        dummies = pd.get_dummies(self._raw_train_data)
+        y = dummies.iloc[: ,[-4]] # last column after get dummies
+        X = dummies.drop(y, axis=1)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=True)
+        # Package data into a dictionary
+        return {
+          'X_train': X_train, 'y_train': y_train,
+          'X_test': X_test, 'y_test': y_test
+        }
+
+    @property
+    def shape(self):
+        return self._raw_train_data.shape
+    
