f3da80713326d0036df0cc551c55b28a6c463107
Eli Simhayev <elisimhayev@gmail.com>
2018-12-15 23:31:34 +0200

    added letter_recognition dataset

--- assignment1/datasets/__init__.py
+++ assignment1/datasets/__init__.py
@@ -2,3 +2,4 @@ from datasets.adult import AdultDataset
 from datasets.abalone import AbaloneDataset
 from datasets.arrhythmia import ArrhythmiaDataset
 from datasets.balance_scale import BalanceScaleDataset
+from datasets.letter_recognition import LetterRecognitionDataset
