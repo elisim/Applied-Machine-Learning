e1d3f48f2a350e0ce1aad29e8c730b38125c61f6
Eli Simhayev <elisimhayev@gmail.com>
2018-12-15 23:57:35 +0200

    added ContraceptiveMethodChoiceDataset

--- assignment1/datasets/__init__.py
+++ assignment1/datasets/__init__.py
@@ -3,3 +3,4 @@ from datasets.abalone import AbaloneDataset
 from datasets.arrhythmia import ArrhythmiaDataset
 from datasets.balance_scale import BalanceScaleDataset
 from datasets.letter_recognition import LetterRecognitionDataset
+from datasets.contraceptive_method_choice import ContraceptiveMethodChoiceDataset
