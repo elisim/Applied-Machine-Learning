6790284b6a86897b1d311b5c261819ca217181f6
tyitzhak <tal_yitzhak@intuit.com>
2018-12-25 16:08:12 +0200

    fixed test missing var and formatting

--- assignment2/utils.py
+++ assignment2/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-def push_results_to_kaggle(pred, fname, msg):
+def push_results_to_kaggle(pred, fname, msg,test):
     ids = np.arange(1, test.shape[0]+1)
     xg_df = pd.DataFrame({"Id" : ids, "Class" : pred})
     xg_df.to_csv("out/"+fname, index=False)
