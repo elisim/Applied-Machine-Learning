e8b60d2f44f1012187229fedc15489992dba24fc
Tal Yitzhak <talyitzhak100@gmail.com>
2018-12-25 16:54:15 +0200

    Update utils.py

--- assignment2/utils.py
+++ assignment2/utils.py
@@ -5,6 +5,6 @@ import os
 def push_results_to_kaggle(pred, fname, msg):
     ids = np.arange(1, pred.shape[0]+1)
     xg_df = pd.DataFrame({"Id" : ids, "Class" : pred})
-    xg_df.to_csv("out/%s"% (fname), index=False)
-    os.system('kaggle competitions submit -c bgu2018 -f out/%s -m "%s"'% (fname, msg))
+    xg_df.to_csv("out/{}".format(fname), index=False)
+    os.system('kaggle competitions submit -c bgu2018 -f out/{} -m "{}"'.format(fname, msg))
 
